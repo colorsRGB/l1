3f4459ca3cdfc5525f928e27cecf0849c4d38851
colorsRGB <redcoreart@gmail.com>
2017-04-25 02:09:56 +0200

    l1 first work first change

---
+++ l1.js
@@ -0,0 +1,11 @@
+function myFunction()
+{
+var name=prompt("Пожалуйста, введите число","0");
+  if (name!=null)
+    {
+      if((name%2)==0)
+        alert("чет");
+          else
+            alert("нечет");
+    }
+}
